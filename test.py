6082ad38c752c8a5270c84ed56881bee2881f67d
teamdhil <106378586+teamdhil@users.noreply.github.com>
2022-06-25 17:43:27 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+model = load_model('model_vgg16.h5')
+img = image.load_img('<imgURL>', target_size=(224, 224)) #replace <imgURL> with any image url
+x = image.img_to_array(img)
+x = np.expand_dims (x, axis=0)
+img_data = preprocess_input(x)
+classes = model.predict(img_data)
